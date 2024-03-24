f5fb12ca8df01041391b94cbd06cf462c8007ad4
Ramakrishna chennuri <ramakrishna.chennuri@archents.com>
2024-03-24 16:29:46 +0530

    first commit

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const scrollTop = window.scrollY;
+    if (scrollTop > 20) {
+      const menu=document.getElementById('menu')
+      menu?.classList.add('top-fixed')
+    }
+    else{
+      const menu=document.getElementById('menu')
+      menu?.classList.remove('top-fixed')
+    }
+  }
+  closeDropdownOnSmallScreen() {
+    // Check if the window width is less than a certain breakpoint (e.g., 768 pixels)
+    if (window.innerWidth < 768) {
+      const navbarToggler = document.querySelector('.navbar-toggler');
+      if (navbarToggler?.classList.contains('collapsed')) {
+        // If the menu is already collapsed, do nothing
+        return;
+      }
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      if (navbarCollapse?.classList.contains('show')) {
+        // If the menu is expanded, collapse it
+        navbarCollapse.classList.remove('show');
+        navbarToggler?.classList.add('collapsed');
+      }
+    }
+  }
 }
